cb7921750ae507a77144bc491c0aeeacd1606561
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-11-11 10:37:39 +0100

    created all component

---
+++ my-recipes/src/app/recipes/recip-detail/recip-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recip-detail',
+  templateUrl: './recip-detail.component.html',
+  styleUrls: ['./recip-detail.component.css']
+})
+export class RecipDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
