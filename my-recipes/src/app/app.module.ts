ac32d6d10c26e93566ad98462c33ecbb574a31f0
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-11-22 11:24:30 +0100

    Fixed

--- my-recipes/src/app/app.module.ts
+++ my-recipes/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipDetailComponent } from './recipes/recip-detail/recip-detail.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
@@ -16,7 +16,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipDetailComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent
--- my-recipes/src/app/models/ingredient.model.ts
+++ my-recipes/src/app/models/ingredient.model.ts
@@ -1,12 +1,11 @@
 export class IngredientModel{
-
     public id:number;
     public name:string;
     public amount:number;
 
 
     constructor(name:string,amount:number){
-            this.name=name;
-            this.amount=amount;
+        this.name=name;
+        this.amount=amount;
     }
 }
